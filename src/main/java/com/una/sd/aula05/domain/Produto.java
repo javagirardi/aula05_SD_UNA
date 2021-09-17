52973b95202ff0e6608c1bcd3b488afb23dbf748
javagirardi <javan.girardi@gmail.com>
2021-09-17 19:33:46 -0300

    criando o GITHUB

--- src/main/java/com/una/sd/aula05/domain/Produto.java
+++ src/main/java/com/una/sd/aula05/domain/Produto.java
@@ -1,2 +1,66 @@
-package com.una.sd.aula05.domain;public class Produto {
+package com.una.sd.aula05.domain;
+
+import java.util.Objects;
+
+public class Produto {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double valor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return id.equals(produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", valor=" + valor +
+                '}';
+    }
 }
